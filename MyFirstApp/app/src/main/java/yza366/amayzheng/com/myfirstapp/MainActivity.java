4d7340a2cf6d9593551e31fc7d3c90aa78721785
John Zheng <jz31337@hotmail.ca>
2018-09-15 14:29:51 -0700

    first commit

---
+++ MyFirstApp/app/src/main/java/yza366/amayzheng/com/myfirstapp/MainActivity.java
@@ -0,0 +1,60 @@
+package yza366.amayzheng.com.myfirstapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addButton = (Button) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+                resultTextView.setText(result + "");
+            }
+        });
+
+        Button secondActivityButton = (Button) findViewById(R.id.secondActivityButton);
+        secondActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                //show how to pass information to second activity
+                startActivity(startIntent);
+
+            }
+        });
+
+        Button googleButton = (Button) findViewById(R.id.googleButton);
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "http://www.google.com";
+                Uri webAddress = Uri.parse(google);
+
+                Intent goToGoogle = new Intent(Intent.ACTION_VIEW, webAddress);
+                if(goToGoogle.resolveActivity(getPackageManager()) != null){
+                    startActivity(goToGoogle);
+                }
+            }
+        });
+
+    }
+}
---
+++ MyFirstApp/app/src/main/java/yza366/amayzheng/com/myfirstapp/SecondActivity.java
@@ -0,0 +1,13 @@
+package yza366.amayzheng.com.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
